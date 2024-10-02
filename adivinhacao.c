88fa271098ceb3e2bc3abda144107b76262c359c
Luka Barth <lukadbarth@hotmail.com>
2024-10-02 20:35:15 -0300

    jogo da adivinhação final primeiro módulo

---
+++ adivinhacao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("*******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhação *\n");
+    printf("*******************************************\n");
+
+    int numeroSecreto = 42;
+
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+}
