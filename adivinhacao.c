b07935e15ea1e9792311941ae1806153c7953623
Luka Barth <lukadbarth@hotmail.com>
2024-10-03 20:26:57 -0300

    jogo da adivinhação final segundo módulo

--- adivinhacao.c
+++ adivinhacao.c
@@ -8,8 +8,40 @@ int main() {
     int numeroSecreto = 42;
 
     int chute;
+    int tentativas = 1;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
+    while (1) {
+
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+
+        if(chute < 0) {
+            printf("Você não pode digitar números negativos!\n");
+            continue;
+        }
+
+        int acertou = chute == numeroSecreto;
+        int maior = chute > numeroSecreto;
+        
+        if(acertou) {
+            printf("Parabéns! Você acertou!\n");
+
+            break;
+        } 
+        
+        else if (maior) {
+            printf("Seu chute foi maior que o número secreto\n");
+        }
+        
+        else {
+            printf("Seu chute foi menor que o número secreto\n");
+        }
+
+        tentativas++;
+    }
+
+    printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas!", tentativas);
 }
